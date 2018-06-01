4a49e070849bd2e345a8d1d027a6d9460c85dbdc
sdana <seth.dana@gmail.com>
2018-06-01 11:52:59 -0500

    added css flexbox to article, added random array generator to test flexbox

--- main.js
+++ main.js
@@ -23,6 +23,13 @@ const jobObjectBuilder = (enumTF, writeTF, businessName, positionWorked, dateSta
         })
     return jobObject   
 }
+require('require')
+var randomWords = require('random-words');
+let testArray = []
+for (let i=0; i<100; i++) {
+    testArray[i] = jobObjectBuilder(true, false, `${i}test`, `${i}test`, `${i}test`, `${i}test`)
+}
+
 const jobObjectDiv = document.querySelector("#jobs-list-div")
 const arringtonVineyard = jobObjectBuilder(true, false, "Arrington Vineyard", "Cellar Hand", "08/2017", "05/2018")
 const hollandHouse = jobObjectBuilder(true, false, "Holland House", "Expo", "Forever", "and Ever")
@@ -33,19 +40,33 @@ const articleTagElement = document.createElement("article")
 articleTagElement.setAttribute("id", "article-tag-element")
 const articleTagElementObject = document.getElementById("article-tag-element")
 let breakElement = document.createElement("br")
-
+let headlineElement = document.createElement("h3")
 jobObjectDiv.appendChild(articleTagElement)
-let listCounter = 1
-for (let j = 0; j < jobsArray.length; j++) {
+
+// let listCounter = 0
+// for (let j = 0; j < jobsArray.length; j++) {
+//     let jobListElement = document.createElement("ul")
+//     jobListElement.setAttribute("id", `list${listCounter}`)
+//     articleTagElement.appendChild(jobListElement)
+//     for (let i in jobsArray[j]) {
+//         let theValue = `${i}: ${jobsArray[j][i]}`
+//         let listItem = document.createElement("li")
+//         listItem.innerHTML = `${theValue}`
+//         jobListElement.appendChild(listItem)
+//     }
+//     listCounter++
+// }
+
+let listCounter = 0
+for (let j = 0; j < testArray.length; j++) {
     let jobListElement = document.createElement("ul")
     jobListElement.setAttribute("id", `list${listCounter}`)
     articleTagElement.appendChild(jobListElement)
-    for (let i in jobsArray[j]) {
-        let theValue = `${i}: ${jobsArray[j][i]}`
+    for (let i in testArray[j]) {
+        let theValue = `${i}: ${testArray[j][i]}`
         let listItem = document.createElement("li")
         listItem.innerHTML = `${theValue}`
         jobListElement.appendChild(listItem)
     }
-    document.getElementById(`list${listCounter}`).appendChild(breakElement)
     listCounter++
 }
